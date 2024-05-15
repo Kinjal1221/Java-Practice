7b1063f1ae6972ae5474d63ff6dfadbc0b3b1884
Kinjal1221 <kinjaltailor12@gmail.com>
2024-05-15 21:55:28 +0530

    15-05-2024

---
+++ IfCondition.java
@@ -0,0 +1,23 @@
+package com.core;
+
+import java.util.Scanner;
+
+public class IfCondition {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter A : ");
+		a=sc.nextInt();
+		System.out.print("Enter B : ");
+		b=sc.nextInt();
+		if(a>b)
+		{
+			System.out.println("A is Max number");
+		}
+		else
+		{
+			System.out.println("B is Max number");
+		}
+	}
+}
